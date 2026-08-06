1a4bdb12ff953bbd23a704215339ed9e25d2b45c
sssb1234 <sanjaybevinkatti1@gmail.com>
2026-08-06 13:50:09 +0530

    Log environment variables and database connection info
    Added console logs to display environment variables and database connection details.

--- src/db/index.ts
+++ src/db/index.ts
@@ -10,4 +10,10 @@ const knexConfig = require('./knexfile')[env];
 
 const db = knex(knexConfig);
 
+console.log("NODE_ENV =", process.env.NODE_ENV);
+console.log("DATABASE_URL exists =", !!process.env.DATABASE_URL);
+console.log("DB_HOST =", process.env.DB_HOST);
+
+console.log(knexConfig.connection);
+
 export default db;
